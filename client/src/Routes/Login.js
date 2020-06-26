018f8398faa1d5f8278df9c45720b240d6503ede
Edgar Jr San Martin <edgar.sanmartinjr@gmail.com>
2020-06-26 05:35:32 -0400

    Added UI for login and signup forms

--- client/src/Routes/Login.js
+++ client/src/Routes/Login.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
   return(
-    <h1>Login</h1>
+    <div className="my-card">
+      <div className="card auth-card input-field">
+        <h2 className="logo-font">Instagram</h2>
+        <input
+          type="text"
+          placeholder="email"
+        />
+        <input
+          type="text"
+          placeholder="password"
+        />
+        <button className="btn waves-effect waves-light #64b5f6 blue lighten-2">Login</button>
+          <p>
+            <Link to="/signup">New around here?</Link>
+          </p>
+      </div>
+    </div>
   );
 }
 
--- client/src/Routes/SignUp.js
+++ client/src/Routes/SignUp.js
@@ -1,8 +1,29 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 const SignUp = () => {
   return(
-    <h1>Sign Up</h1>
+    <div className="my-card">
+      <div className="card auth-card input-field">
+        <h2 className="logo-font">Instagram</h2>
+          <input
+            type="text"
+            placeholder="name"
+          />
+        <input
+          type="text"
+          placeholder="email"
+        />
+        <input
+          type="text"
+          placeholder="password"
+        />
+        <button className="btn waves-effect waves-light #64b5f6 blue lighten-2">Sign Up</button>
+        <p>
+          <Link to="/login">Already have an account?</Link>
+        </p>
+      </div>
+    </div>
   );
 }
 
