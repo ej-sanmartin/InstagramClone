20bf6c4de269f3a70367e6bf8b07da106919212d
Edgar Jr San Martin <edgar.sanmartinjr@gmail.com>
2020-06-28 04:17:00 -0400

    connect signup form to backend

--- client/src/Routes/SignUp.js
+++ client/src/Routes/SignUp.js
@@ -1,7 +1,50 @@
-import React from 'react';
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import M from 'materialize-css';
 
 const SignUp = () => {
+  const history = useHistory();
+
+  const emailChecker = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+  const [ name, setName ] = useState("");
+  const [ password, setPassword ] = useState("");
+  const [ email, setEmail ] = useState("");
+  const PostData = () => {
+    if(!emailChecker.test(email)){
+      return M.toast({
+        html: "Invalid Email",
+        classes: "#c62828 red darken-3"
+      });
+    }
+    fetch('/signup', {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        name,
+        password,
+        email
+      })
+    })
+    .then(res => res.json())
+    .then(data => {
+      if(data.error){
+        M.toast({
+          html: data.error,
+          classes: "#c62828 red darken-3"
+        });
+      } else {
+        M.toast({
+          html: data.message,
+          classes: "#43a047 green darken-1"
+        });
+        history.push('/login');
+      }
+    })
+  }
+
   return(
     <div className="my-card">
       <div className="card auth-card input-field">
@@ -9,16 +52,24 @@ const SignUp = () => {
           <input
             type="text"
             placeholder="name"
+            value={name}
+            onChange={(e) =>setName(e.target.value)}
           />
         <input
           type="text"
           placeholder="email"
+          value={email}
+          onChange={(e) =>setEmail(e.target.value)}
         />
         <input
           type="text"
           placeholder="password"
+          value={password}
+          onChange={(e) =>setPassword(e.target.value)}
         />
-      <button className="btn waves-effect waves-light #64b5f6 blue darken-1">Sign Up</button>
+      <button className="btn waves-effect waves-light #64b5f6 blue darken-1"
+              onClick={() => PostData()}
+        >Sign Up</button>
         <p>
           <Link to="/login">Already have an account?</Link>
         </p>
